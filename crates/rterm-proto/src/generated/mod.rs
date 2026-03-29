63f92528aeb9d41afcd8ba951bf8a4b06b2831cc
Shuoze Li <szsuzyli@gmail.com>
2026-03-29 18:04:21 +0000

    style: fmt

--- crates/rterm-proto/src/generated/mod.rs
+++ crates/rterm-proto/src/generated/mod.rs
@@ -1,4 +1,11 @@
-#[allow(unused_imports, dead_code, unused_variables, unused_mut, clippy::all, non_snake_case)]
+#[allow(
+    unused_imports,
+    dead_code,
+    unused_variables,
+    unused_mut,
+    clippy::all,
+    non_snake_case
+)]
 mod rterm_generated;
 
 pub use rterm_generated::*;
